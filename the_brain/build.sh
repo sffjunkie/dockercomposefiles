d3e5651fd832f4a60a2d2d13beec82abb9184249
Simon Kennedy <sffjunkie+code@gmail.com>
2021-04-07 20:40:07 +0100

    Script fixed to copy to build directory

--- the_brain/build.sh
+++ the_brain/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm -rf ./build/
 mkdir ./build/
-command cp main.yaml docker-compose.yaml
+cp main.yaml ./build/docker-compose.yaml
 find . ../common/ -type f \( -iname "*.yaml" -not \( -iname "docker-compose.yaml" -or -iname "main.yaml" \) \) -exec cat {} >> ./build/docker-compose.yaml \;
 find . ../common/  -type f \( -iname ".*-env" -and -not -path "./build/*" \) -exec cp {} ./build/ \;

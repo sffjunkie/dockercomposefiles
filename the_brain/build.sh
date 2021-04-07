0170110ec62bd2569e762caa1eef97f1500fe11f
Simon Kennedy <sffjunkie+code@gmail.com>
2021-04-07 19:11:54 +0100

    Updated build script to create a build directory and copy .*-env files

--- the_brain/build.sh
+++ the_brain/build.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-command cp main.yaml docker-compose.yaml && find . ../common/ -type f \( -iname "*.yaml" -not \( -iname "docker-compose.yaml" -or -iname "main.yaml" \) \) -exec cat {} >> docker-compose.yaml \;
+rm -rf ./build/
+mkdir ./build/
+command cp main.yaml docker-compose.yaml
+find . ../common/ -type f \( -iname "*.yaml" -not \( -iname "docker-compose.yaml" -or -iname "main.yaml" \) \) -exec cat {} >> ./build/docker-compose.yaml \;
+find . ../common/  -type f \( -iname ".*-env" -and -not -path "./build/*" \) -exec cp {} ./build/ \;
